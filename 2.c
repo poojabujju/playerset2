91ba31fe61dc0fbfb24f2e0172414939515bf17a
Pooja <31477611+poojabujju@users.noreply.github.com>
2018-03-14 15:40:06 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -2,12 +2,21 @@
 #include<string.h>
 void main()
 {
-int N,K,i,j,a[10],l;
+int N,K,i,t,a[10];
 printf("Enter N and K :");
 scanf("%d %d",&N,&K);
 for(i=0;i<N;i++)
 {
     scanf("%d",&a[i]);
 }
-l=strlen(a);
-for(i=l-1;i;)
+    for(i=0;i<K;i++)
+    {
+        t=a[i];
+        a[i]=a[i+1];
+        a[i+1]=t;
+    }
+    for(i=0;i<N;i++)
+    {
+    printf("%d",a[i]);
+    }
+}
