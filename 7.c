6ebf4815a3a053e2588a07122e1a8323f231c776
Pooja <31477611+poojabujju@users.noreply.github.com>
2018-03-16 15:30:00 +0530

    Update 7.c

--- 7.c
+++ 7.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 void main()
 {
-    int a,b,c;
+    int a,b,c,i;
     printf("Enter two Numbers : ");
     scanf("%d %d",&a,&b);
-    for(int i=2;i<=b;i++)
+    for(i=2;i<=b;i++)
     {
     if(a%i==0 && b%i==0)
     {
-        printf("\t%d",i);
+        printf("\t%d\n",i);
         
     }
-    }
-    printf("Smallest divisor : %d",i)
-    }
+    
+}
+}
